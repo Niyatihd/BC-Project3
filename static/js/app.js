31a8d6e9188b3156e5f0bee0f5d894c9f9bcf8d5
ninja85 <niyatihd@gmail.com>
2018-07-21 15:29:16 -0700

    updated app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,11 @@
 /****** TEST ******/
 console.log("app.js link Works!!!!!!!!!!!")
+
+//##########################################################
+//Post, get values to and from app.py and render sliderplot 
+//##########################################################
+
+function updatePlot(ddl1) {
+    console.log("creating historical plot with prediction");
+}
+
