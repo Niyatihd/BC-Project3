5cf35f5e5966339aace77afce7c2a7732f760b71
ninja85 <niyatihd@gmail.com>
2018-07-23 16:30:17 -0700

    work in progress on plotly prediction plot

--- static/js/app.js
+++ static/js/app.js
@@ -6,6 +6,7 @@ console.log("app.js link Works!!!!!!!!!!!")
 //##########################################################
 
 function updatePlot(ddl1) {
+    var userSel = ddl1;
     // console.log("creating historical plot with prediction");
     console.log(ddl1);
     var selectedArea = (ddl1 === "Select Area")?"":ddl1;
@@ -52,9 +53,114 @@ function updatePlot(ddl1) {
         empCoeffid.appendChild(todoListItem1);
         empCoeffid.appendChild(todoListItem2);
         empCoeffid.appendChild(todoListItem3);
+    });
+
+        function buildPlot() {
+            Plotly.d3.json("/plotlyData", function(error, response) {
+                if (error) return console.warn(error);
+                console.log(response);
+              // Grab values from the response json object to build the plots
+              var year = response[userSel].year;
+              var households = response[userSel].Households;
+              var income = response[userSel].Income;
+              var jobs = response[userSel].Jobs;
+              var medianHomePrice = response[userSel]["Median Home Price"];
+              console.log(userSel, year, households, income, jobs, medianHomePrice);
+              
+            //   function init() {
+                var trace1 = {
+                    type: "scatter",
+                    mode: "lines",
+                    name: name,
+                    x: year,
+                    y: households,
+                    line: {
+                    color: "#17BECF"
+                    }
+                };
+        
+                var trace2 = {
+                    type: "scatter",
+                    mode: "lines",
+                    name: name,
+                    x: year,
+                    y: income,
+                    line: {
+                    color: "red"
+                    }
+                };
+        
+                var trace3 = {
+                    type: "scatter",
+                    mode: "lines",
+                    name: name,
+                    x: year,
+                    y: jobs,
+                    line: {
+                    color: "blue"
+                    }
+                };
+        
+                var trace4 = {
+                    type: "scatter",
+                    label: "medianHomePrice",
+                    mode: "lines",
+                    name: name,
+                    x: year,
+                    y: medianHomePrice,
+                    line: {
+                    color: "black"
+                    }
+                };
+            
+                var data = [trace1, trace2, trace3, trace4];
+            
+                //   var layout = {
+                //     title: `${stock} closing prices`,
+                //     xaxis: {
+                //       range: [startDate, endDate],
+                //       type: "date"
+                //     },
+                //     yaxis: {
+                //       autorange: true,
+                //       type: "linear"
+                //     }
+                //   };
+            
+                Plotly.newPlot("plotlyPlot", data); //layout);
+                })
+        
+            // function updatePlotly(newdata) {
+            //     var LINE = document.getElementById("plotlyPlot");
+            //     Plotly.restyle(LINE, "y", [newdata]);
+            //     }
+        
+            // function getData(dataset) {
+            //     var data = [];
+            //     switch (dataset) {
+            //     case "dataset1":
+            //         data = [1, 2, 3, 39];
+            //         break;
+            //     case "dataset2":
+            //         data = [10, 20, 30, 37];
+            //         break;
+            //     case "dataset3":
+            //         data = [100, 200, 300, 23];
+            //         break;
+            //     default:
+            //         data = [30, 30, 30, 11];
+            //     }
+            //     updatePlotly(data);
+            //     }
+        
+            // init();
+            // });
+        }
+
+        buildPlot();
 
     });
-});
+
 }
 
 var images = [
--- static/js/plotlyplot.js
+++ static/js/plotlyplot.js
@@ -17,68 +17,97 @@ function buildPlot() {
       var medianHomePrice = response.Alameda["Median Home Price"];
       console.log(year, households, income, jobs, medianHomePrice);
       
-      var trace1 = {
-        type: "scatter",
-        mode: "lines",
-        name: name,
-        x: year,
-        y: households,
-        line: {
-          color: "#17BECF"
-        }
-      };
+      function init() {
+        var trace1 = {
+            type: "scatter",
+            mode: "lines",
+            name: name,
+            x: year,
+            y: households,
+            line: {
+            color: "#17BECF"
+            }
+        };
 
-      var trace2 = {
-        type: "scatter",
-        mode: "lines",
-        name: name,
-        x: year,
-        y: income,
-        line: {
-          color: "red"
-        }
-      };
+        var trace2 = {
+            type: "scatter",
+            mode: "lines",
+            name: name,
+            x: year,
+            y: income,
+            line: {
+            color: "red"
+            }
+        };
 
-      var trace3 = {
-        type: "scatter",
-        mode: "lines",
-        name: name,
-        x: year,
-        y: jobs,
-        line: {
-          color: "blue"
-        }
-      };
+        var trace3 = {
+            type: "scatter",
+            mode: "lines",
+            name: name,
+            x: year,
+            y: jobs,
+            line: {
+            color: "blue"
+            }
+        };
 
-      var trace4 = {
-        type: "scatter",
-        label: "medianHomePrice",
-        mode: "lines",
-        name: name,
-        x: year,
-        y: medianHomePrice,
-        line: {
-          color: "black"
+        var trace4 = {
+            type: "scatter",
+            label: "medianHomePrice",
+            mode: "lines",
+            name: name,
+            x: year,
+            y: medianHomePrice,
+            line: {
+            color: "black"
+            }
+        };
+    
+        var data = [trace1, trace2, trace3, trace4];
+    
+        //   var layout = {
+        //     title: `${stock} closing prices`,
+        //     xaxis: {
+        //       range: [startDate, endDate],
+        //       type: "date"
+        //     },
+        //     yaxis: {
+        //       autorange: true,
+        //       type: "linear"
+        //     }
+        //   };
+    
+        Plotly.newPlot("plotlyPlot", data); //layout);
         }
-      };
-  
-      var data = [trace1, trace2, trace3, trace4];
-  
-    //   var layout = {
-    //     title: `${stock} closing prices`,
-    //     xaxis: {
-    //       range: [startDate, endDate],
-    //       type: "date"
-    //     },
-    //     yaxis: {
-    //       autorange: true,
-    //       type: "linear"
+
+    // function updatePlotly(newdata) {
+    //     var LINE = document.getElementById("plotlyPlot");
+    //     Plotly.restyle(LINE, "y", [newdata]);
     //     }
-    //   };
-  
-      Plotly.newPlot("plotlyPlot", data); //layout);
-  
+
+    // function getData(dataset) {
+    //     var data = [];
+    //     switch (dataset) {
+    //     case "dataset1":
+    //         data = [1, 2, 3, 39];
+    //         break;
+    //     case "dataset2":
+    //         data = [10, 20, 30, 37];
+    //         break;
+    //     case "dataset3":
+    //         data = [100, 200, 300, 23];
+    //         break;
+    //     default:
+    //         data = [30, 30, 30, 11];
+    //     }
+    //     updatePlotly(data);
+    //     }
+
+    init();
     });
-  }
+}
   
-  buildPlot();
+
+
+  
+// buildPlot();
